f3669d5592ae91f8d456ab41daebd19649e3c3d9
tomeryoel <96529795+tomeryoel@users.noreply.github.com>
2023-11-01 10:16:55 +0200

    adding the first files to git

---
+++ src/main/java/com/moviesAndSeries/moviesAndSeries/MoviesAndSeriesApplication.java
@@ -0,0 +1,14 @@
+package com.moviesAndSeries.moviesAndSeries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MoviesAndSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAndSeriesApplication.class, args);
+	}
+}
